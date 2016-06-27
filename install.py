d6c9c9cb3f4655e78ffd2a3d76167353cd9018d6
David Becvarik <dbecvari@redhat.com>
2016-06-27 14:45:42 +0200

    added missing imports

--- install.py
+++ install.py
@@ -1,9 +1,13 @@
 from zipfile import ZipFile
+from cct.module import Module
+
+import os
 
 class Install(Module):
 
     def install(self):
-        eap_zip_path = os.path.join("/tmp/script/sources/", self.artifacts['eap.zip'])
+        eap_zip_path = os.path.join("/tmp/script/sources/",
+                                    self.artifacts['eap.zip'])
         zip = ZipFile(eap_zip_path)
         zip.extractall(os.getenv("JBOSS_HOME"))
-        #FIXME permissions
+        # FIXME permissions
