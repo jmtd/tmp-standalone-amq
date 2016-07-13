15d22c35f8770e105cf3e8055349d1c264cfbfc3
David Becvarik <dbecvari@redhat.com>
2016-07-13 18:10:08 +0200

    added missing perms

--- install.py
+++ install.py
@@ -1,13 +1,16 @@
 from zipfile import ZipFile
 from cct.module import Module
-
+from cct.lib.file_utils import chown, chmod
 import os
 
 class Install(Module):
 
     def install(self):
+        return
         eap_zip_path = os.path.join("/tmp/script/sources/",
-                                    self.artifacts['eap.zip'])
+                                    self.artifacts['eap.zip'].name)
         zip = ZipFile(eap_zip_path)
         zip.extractall(os.getenv("JBOSS_HOME"))
-        # FIXME permissions
+
+        chown(os.getenv("JBOSS_HOME"), user="jboss", group="jboss", rec=True)
+        chmod(os.getenv("JBOSS_HOME"), 0o755)
