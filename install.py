7bed91fa19dd04a32935ffb2300b8dc91483c70f
Marek Goldmann <marek.goldmann@gmail.com>
2016-07-18 20:06:25 +0200

    Add actually working EAP 7 install module

--- install.py
+++ install.py
@@ -1,15 +1,9 @@
-from zipfile import ZipFile
-from cct.module import Module
-from cct.lib.file_utils import chown, chmod
-import os
+from cct.module.jboss import JBossInstall
 
-class Install(Module):
+class Install(JBossInstall):
 
     def install(self):
-        eap_zip_path = os.path.join("/tmp/scripts/sources/",
-                                    self.artifacts['eap.zip'].name)
-        zip = ZipFile(eap_zip_path)
-        zip.extractall(os.getenv("JBOSS_HOME"))
+        self._unpack_distribution()
+        self._apply_patches(["jbeap4410.zip"])
+        self._change_owner()
 
-        chown(os.getenv("JBOSS_HOME"), user="jboss", group="jboss", recursive=True)
-        chmod(os.getenv("JBOSS_HOME"), 0o755)
