363943531ca06a3c98e36eedc12632d4cc12d09b
Marek Goldmann <marek.goldmann@gmail.com>
2016-07-19 09:52:50 +0200

    Add proper header

--- install.py
+++ install.py
@@ -1,3 +1,11 @@
+"""
+Copyright (c) 2015 Red Hat, Inc
+All rights reserved.
+
+This software may be modified and distributed under the terms
+of the MIT license. See the LICENSE file for details.
+"""
+
 from cct.module.jboss import JBossInstall
 
 class Install(JBossInstall):
